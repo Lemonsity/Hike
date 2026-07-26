37bf76a6c8e4684fb78b0b7833257f7e3e410106
Lemon <38927629+Lemonsity@users.noreply.github.com>
2026-07-25 22:12:24 -0400

    Add import, try happstack-lite

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,35 @@
 module Main (main) where
 
+import Control.Applicative ((<$>), optional)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import Data.Text.Lazy (unpack)
+import Happstack.Lite
+import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
+import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
 import Database.HDBC.Sqlite3
 
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  _ <- connectSqlite3 "database"
-  putStrLn "Hello Again!"
+main = serve Nothing myApp
+
+myApp :: ServerPart Response
+myApp = msum
+  [ dir "echo"    $ echo
+  , dir "query"   $ queryParams
+  , dir "form"    $ formPage
+  , dir "fortune" $ fortune
+  , dir "files"   $ fileServing
+  , dir "upload"  $ upload
+  , homePage
+  ]
+
+
+-- main :: IO ()
+-- main = do
+--   putStrLn "Hello, Haskell!"
+--   _ <- connectSqlite3 "database"
+--   putStrLn "Hello Again!"
