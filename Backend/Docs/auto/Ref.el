285c8c342bf4a8c19020fa8f59654cd6e5b6e70b
M <38927629+YZmarkS@users.noreply.github.com>
2026-08-04 21:28:29 -0400

    Prepare for "monorepo"
    I want to put design docs in the same repo
    But putting it in the docs of the backend app seems off
    Moving things, may turn into a monorepo in the future

---
+++ Backend/Docs/auto/Ref.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "Ref"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "10.1145/1040305.1040306"
+    "10.1145/1047659.1040306"))
+ '(or :bibtex :latex))
+
